92be2e5b8427b557f05eb5e5f2fd4563faecde28
Tomas Bozzo <tomasbozzo@gmail.com>
2019-02-04 21:00:48 +0100

    Create basic Spring Boot project

---
+++ src/main/java/com/tomasbozzo/spring/springbootpreview/SpringBootPreviewApplication.java
@@ -0,0 +1,14 @@
+package com.tomasbozzo.spring.springbootpreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPreviewApplication.class, args);
+    }
+
+}
+
