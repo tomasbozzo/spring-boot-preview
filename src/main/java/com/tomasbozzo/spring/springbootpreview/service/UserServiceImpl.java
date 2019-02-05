8e3710c56b873ca7479da8f616537559a78d21ba
Tomas Bozzo <tomasbozzo@gmail.com>
2019-02-05 22:18:29 +0100

    Spring cache

--- src/main/java/com/tomasbozzo/spring/springbootpreview/service/UserServiceImpl.java
+++ src/main/java/com/tomasbozzo/spring/springbootpreview/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.tomasbozzo.spring.springbootpreview.service;
 
 import com.tomasbozzo.spring.springbootpreview.repository.UserRepository;
 import com.tomasbozzo.spring.springbootpreview.repository.model.UserEntity;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -23,17 +25,20 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @CacheEvict(value = "users", key = "#entity.id")
     public UserEntity insert(UserEntity entity) {
         return repository.save(entity);
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
+    @CacheEvict(value = "users", key = "#id")
     public void delete(Long id) {
         repository.deleteById(id);
     }
 
     @Override
+    @Cacheable(value = "users", key = "#id")
     public Optional<UserEntity> findById(Long id) {
         return repository.findById(id);
     }
---
+++ src/main/java/com/tomasbozzo/spring/springbootpreview/service/configuration/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package com.tomasbozzo.spring.springbootpreview.service.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+}
